d52f6b54a7d7a819abc2ad75831e8d26464305d6
Robert Reed <robert.mc.reed@gmail.com>
2021-05-08 18:43:09 -0700

    feat: add path constants

---
+++ library/constants.py
@@ -0,0 +1,7 @@
+import os
+from os.path import join, dirname
+
+root_path = join(dirname(__file__), '..')
+models_path = join(root_path, 'models')
+config_path = join(root_path, 'config.json')
+default_config_path = join(root_path, 'config.default.json')
