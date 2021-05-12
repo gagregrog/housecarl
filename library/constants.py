1e87b214b4486afdbb9d461fcc283135d7b09d3f
Robert Reed <robert.mc.reed@gmail.com>
2021-05-12 01:08:46 -0700

    refactor: move config

--- library/constants.py
+++ library/constants.py
@@ -1,4 +1,3 @@
-import os
 from os.path import join, dirname
 
 root_path = join(dirname(__file__), '..')
@@ -6,4 +5,4 @@ models_path = join(root_path, 'models')
 mobilenet_path = join(models_path, 'mobilenet')
 yolo_path = join(models_path, 'yolo')
 config_path = join(root_path, 'config.json')
-default_config_path = join(root_path, 'config.default.json')
+default_config_path = join(root_path, 'library', 'config.default.json')
