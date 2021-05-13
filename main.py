542b4c1ef2905fbd1302b108348ec04ebe396060
Robert Reed <robert.mc.reed@gmail.com>
2021-05-12 23:36:08 -0700

    feat: wire up monitor and detector in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from library import utility
 from library.video import RTSP
+from library.monitor import Monitor
 from library.detectors import get_detector
 
 def main():
@@ -11,7 +12,16 @@ def main():
     utility.info('Loading detector...')
     detector = get_detector(config)
 
-    video = RTSP(config=config, frame_handler=lambda frame: detector.process_frame(frame))
+    monitor = Monitor(config)
+
+    def handle_video_frame(frame):
+        detector.process_frame(frame, on_detections=monitor.handle_detections)
+
+    video = RTSP(
+        config=config,
+        frame_handler=handle_video_frame,
+        on_exit=detector.terminate_thread
+    )
 
     utility.info('Starting video stream...')
     video.start()
