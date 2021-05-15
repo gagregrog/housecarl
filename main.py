7c4828fe9cca244347265cd7315d4b8c4dd87251
Robert Reed <robert.mc.reed@gmail.com>
2021-05-15 14:32:08 -0700

    feat: catch keyboard interrupts and shutdown
    * feat: catch exceptions and shutdown thread

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import sys
 from library import utility
 from library.video import RTSP
 from library.monitor import Monitor
 from library.detectors import get_detector
 
+detector = None
+
 def main():
+    global detector
     args = utility.get_args()
     config = utility.get_config(args.get('config'))
     utility.info('Loaded configuration:')
@@ -27,4 +31,16 @@ def main():
     video.start()
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except (KeyboardInterrupt, Exception) as exception:
+        if detector:
+            detector.terminate_thread()
+        
+        # don't show error stack if user terminates the script
+        if type(exception).__name__ == 'KeyboardInterrupt':
+            utility.error('\n\n\tKeyboard Interrupt. Shutting down gracefully...')
+            sys.exit(1)
+        else:
+            # raise any other types of exceptions
+            raise exception
