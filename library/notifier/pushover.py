e19ba2df5a12d93921c461abd0651496097520fa
Robert Reed <robert.mc.reed@gmail.com>
2021-05-19 21:40:43 -0700

    feat: respect pushover mock

--- library/notifier/pushover.py
+++ library/notifier/pushover.py
@@ -12,15 +12,21 @@ class Pushover:
         self.verify_tokens(config)
 
     def verify_tokens(self, config):
+        self.mock = False
         self.user_key = config["pushover_user_key"]
         self.app_token = config["pushover_app_token"]
 
-        if not self.user_key:
+        if config.get("mock_pushover"):
+            self.mock = True
+        elif not self.user_key:
             raise Exception("config.pushover_user_key is required")
         elif not self.app_token:
             raise Exception("config.pushover_app_token is required")
 
     def send_push_notification(self, message: str, image: np.ndarray = None):
+        if self.mock:
+            return utility.info('PUSHOVER_MOCK: {}'.format(message))
+
         thread = Thread(target=self.__send_push_notification, args=(message, image))
         thread.start()
 
