26bd27ad446452a78fb0eeb35944e8844105f9c3
Robert Reed <robert.mc.reed@gmail.com>
2021-06-02 08:29:26 -0700

    feat: move pycoral wheel urls to constants

--- housecarl/library/constants.py
+++ housecarl/library/constants.py
@@ -10,3 +10,5 @@ yolo_path = join(models_path, 'yolo')
 config_path = join(root_path, 'config.json')
 default_config_path = join(housecarl_path, 'library', 'config.default.json')
 edge_tpu_path = join(root_path, 'edgetpu_runtime')
+pycoral_rpi_wheel = "https://github.com/google-coral/pycoral/releases/download/v1.0.1/pycoral-1.0.1-cp37-cp37m-linux_armv7l.whl"
+tflite_rpi_wheel = "https://github.com/google-coral/pycoral/releases/download/v1.0.1/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl"
