b7ee58632aed09bca9c8a2b6757d7c6d128d8867
Robert Reed <robert.mc.reed@gmail.com>
2021-06-14 21:01:49 -0700

    feat: templates path in constants

--- housecarl/library/constants.py
+++ housecarl/library/constants.py
@@ -1,14 +1,22 @@
 from os.path import join, dirname, abspath
 
-root_path = abspath(join(dirname(__file__), '..', '..'))
 housecarl_path = abspath(join(dirname(__file__), '..'))
-models_path = join(root_path, 'models')
+root_path = abspath(join(dirname(__file__), '..', '..'))
+
+config_path = join(root_path, 'config.json')
+default_config_path = join(housecarl_path, 'library', 'config.default.json')
+
 recordings_path = join(root_path, 'recordings')
+
+models_path = join(root_path, 'models')
 mobilenet_path = join(models_path, 'mobilenet')
 coral_path = join(models_path, 'coral')
 yolo_path = join(models_path, 'yolo')
-config_path = join(root_path, 'config.json')
-default_config_path = join(housecarl_path, 'library', 'config.default.json')
+
 edge_tpu_path = join(root_path, 'edgetpu_runtime')
+
 pycoral_rpi_wheel = "https://github.com/google-coral/pycoral/releases/download/v1.0.1/pycoral-1.0.1-cp37-cp37m-linux_armv7l.whl"
+
 tflite_rpi_wheel = "https://github.com/google-coral/pycoral/releases/download/v1.0.1/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl"
+
+templates_path = join(root_path, 'library', 'server', 'templates')
